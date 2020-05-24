4d2b2f4cbb888f53b7661b3dc97577b617acd783
hmsk <k.hamasaki@gmail.com>
2020-05-23 18:31:08 -0700

    Show date and original url

--- src/Static/Entry.elm
+++ src/Static/Entry.elm
@@ -2,14 +2,16 @@ module Static.Entry exposing (main)
 
 import Css exposing (..)
 import Html exposing (Html)
-import Html.Styled exposing (article, div, fromUnstyled, h2, text, toUnstyled)
-import Html.Styled.Attributes exposing (css)
+import Html.Styled exposing (a, article, div, fromUnstyled, h2, h3, p, text, toUnstyled)
+import Html.Styled.Attributes exposing (css, href)
 import Json.Decode as D exposing (Decoder)
+import Json.Decode.Extra exposing (datetime)
 import Markdown
 import Regex
 import Siteelm.Html as Html
 import Siteelm.Page exposing (Page, page)
 import Static.View as View
+import Time exposing (..)
 
 
 main : Page Preamble
@@ -22,13 +24,18 @@ main =
 
 
 type alias Preamble =
-    { title : String }
+    { title : String
+    , date : Posix
+    , originalUrl : Maybe String
+    }
 
 
 preambleDecoder : Decoder Preamble
 preambleDecoder =
-    D.map Preamble
+    D.map3 Preamble
         (D.field "title" D.string)
+        (D.field "date" datetime)
+        (D.field "original_url" D.string |> D.maybe)
 
 
 viewHead : Preamble -> String -> List (Html Never)
@@ -49,12 +56,82 @@ viewBody preamble body =
         [ View.header
         , article [ css [ maxWidth (px 1280), margin auto ] ]
             [ h2 [] [ text preamble.title ]
+            , h3 [] [ text <| dateFormat preamble.date ]
+            , linkForOriginal preamble.originalUrl
             , div []
                 [ fromUnstyled <| Markdown.toHtmlWith markedOptions [] processedBody
                 ]
             ]
         , View.footer
         ]
+
+
+dateFormat : Posix -> String
+dateFormat posix =
+    let
+        y =
+            String.fromInt <| toYear utc posix
+
+        m =
+            toHumanMonth <| toMonth utc posix
+
+        d =
+            String.fromInt <| toDay utc posix
+    in
+    String.concat [ m, " ", d, ", ", y ]
+
+
+linkForOriginal : Maybe String -> Html.Styled.Html Never
+linkForOriginal maybeOriginal =
+    case maybeOriginal of
+        Just link ->
+            p []
+                [ text "公開時のURL:"
+                , a [ href link ] [ text link ]
+                ]
+
+        _ ->
+            text ""
+
+
+toHumanMonth : Month -> String
+toHumanMonth month =
+    case month of
+        Jan ->
+            "January"
+
+        Feb ->
+            "Feburary"
+
+        Mar ->
+            "March"
+
+        Apr ->
+            "April"
+
+        May ->
+            "May"
+
+        Jun ->
+            "June"
+
+        Jul ->
+            "July"
+
+        Aug ->
+            "August"
+
+        Sep ->
+            "September"
+
+        Oct ->
+            "October"
+
+        Nov ->
+            "November"
+
+        Dec ->
+            "December"
 
 
 type CustomTagType
