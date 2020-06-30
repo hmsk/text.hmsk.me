75f917a9492ca75a8e82a04b931e788c021b3dd6
hmsk <k.hamasaki@gmail.com>
2020-06-30 01:08:03 -0700

    Use hmsk/elm-css-modern-normalize

--- src/Siteelm/Page.elm
+++ src/Siteelm/Page.elm
@@ -1,8 +1,9 @@
 module Siteelm.Page exposing (Page, page)
 
 import Browser
-import Css exposing (auto, backgroundColor, baseline, bolder, borderBottom3, borderBox, borderStyle, bottom, boxSizing, color, dashed, display, dotted, em, fontFamilies, fontFamily, fontSize, fontWeight, height, hex, hover, inherit, lineHeight, listItem, margin, marginBottom, none, num, outline, outline3, outlineOffset, padding, padding2, padding3, pct, position, px, relative, solid, textDecoration, textDecoration2, textTransform, top, underline, verticalAlign)
-import Css.Global exposing (Snippet, global, selector)
+import Css exposing (backgroundColor, borderBottom3, borderStyle, color, dashed, em, fontFamilies, hex, hover, none, padding2, px, solid, textDecoration)
+import Css.Global exposing (global, selector)
+import Css.ModernNormalize as ModernNormalize
 import Html exposing (Html)
 import Html.Attributes exposing (attribute, href, lang, name, title, type_)
 import Html.Styled exposing (Attribute, fromUnstyled, node, toUnstyled)
@@ -92,6 +93,7 @@ renderPage head body model =
                         , Html.meta [ property "twitter:creator", content "@hmsk" ]
                         , Html.meta [ property "twitter:site", content "@hmsk" ]
                         , toUnstyled globalStyle
+                        , toUnstyled ModernNormalize.globalStyledHtml
                         ]
                 , node "body" [ bodyStyle ] bodyHtml |> toUnstyled
                 ]
@@ -125,103 +127,4 @@ globalStyle =
             ]
         , selector ".dynamicInstagram" [ borderStyle none ]
         ]
-            ++ modernNormalize
-
-
-modernNormalize : List Snippet
-modernNormalize =
-    -- https://github.com/sindresorhus/modern-normalize
-    [ selector "*, *::before, *::after" [ boxSizing borderBox ]
-    , selector ":root"
-        [ Css.property "-moz-tab-size" "4"
-        , Css.property "tab-size" "4"
-        ]
-    , selector "html"
-        [ lineHeight (num 1.15)
-        , Css.property "-webkit-text-size-adjust" "100%"
-        ]
-    , selector "body"
-        [ margin (px 0)
-        , fontFamilies
-            [ "system-ui", "-apple-system", "Segoe UI", "Roboto", "Helvetica", "Arial", "sans-serif", "Apple Color Emoji", "Segoe UI Emoji" ]
-        ]
-    , selector "hr"
-        [ height (px 0)
-        ]
-    , selector "abbr[title]"
-        [ textDecoration2 underline dotted
-        ]
-    , selector "b, strong"
-        [ fontWeight bolder
-        ]
-    , selector "code, kbd, samp, pre"
-        [ fontFamilies [ "SFMono-Regular", "Consolas", "Liberation Mono", "Menlo", "monospace" ]
-        , fontSize (em 1)
-        ]
-    , selector "small"
-        [ fontSize (pct 80)
-        ]
-    , selector "sub,sup"
-        [ fontSize (pct 75)
-        , lineHeight (num 0)
-        , position relative
-        , verticalAlign baseline
-        ]
-    , selector "sub"
-        [ bottom (em -0.25)
-        ]
-    , selector "sup"
-        [ top (em -0.5)
-        ]
-    , selector "button, input, optgroup, select, textarea"
-        [ fontFamily inherit
-        , fontSize (pct 100)
-        , lineHeight (num 1.15)
-        , margin (px 0)
-        ]
-    , selector "button, select"
-        [ textTransform none
-        ]
-    , selector "button, [type='button'], [type='reset'], [type='submit']"
-        [ Css.property "-webkit-appearance" "button"
-        ]
-    , selector "button::-moz-focus-inner, [type='button']::-moz-focus-inner, [type='reset']::-moz-focus-inner, [type='submit']::-moz-focus-inner"
-        [ borderStyle none
-        , padding (px 0)
-        ]
-    , selector "button:-moz-focusring, [type='button']:-moz-focusring, [type='reset']:-moz-focusring, [type='submit']:-moz-focusring"
-        [ Css.property "outline" "1px dotted ButtonText"
-        ]
-    , selector "fieldset"
-        [ padding3 (em 0.35) (em 0.75) (em 0.625)
-        ]
-    , selector "legend"
-        [ padding (px 0) ]
-    , selector "progress"
-        [ verticalAlign baseline ]
-    , selector "[type='number']::-webkit-inner-spin-button, [type='number']::-webkit-outer-spin-button"
-        [ height auto ]
-    , selector "[type='search']"
-        [ outlineOffset (px -2)
-        , Css.property "-webkit-appearance" "textfield"
-        ]
-    , selector "[type='search']::-webkit-search-decoration"
-        [ Css.property "-webkit-appearance" "none"
-        ]
-    , selector "::-webkit-file-upload-button"
-        [ Css.property "font" "inherit"
-        , Css.property "-webkit-appearance" "button"
-        ]
-    , selector "summary"
-        [ display listItem
-        ]
-    ]
-
-
-
-{- buttonText : ColorValue NonMixable
-   buttonText =
-   { value = "ButtonText"
-   , color = Compatible
-   }
--}
+            ++ ModernNormalize.snippets
